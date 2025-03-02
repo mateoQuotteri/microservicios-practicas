61a8f5ea9f032f2d7f50813403ac54bce05dee7e
Mateo <quotterimateo@gmail.com>
2025-03-01 23:36:17 -0300

    2 commit

---
+++ checkout-service/src/main/java/com/checkout/demo/CheckoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.checkout.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutServiceApplication.class, args);
+	}
+
+}
---
+++ products-service/src/test/java/com/products/products_service/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.products.products_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
