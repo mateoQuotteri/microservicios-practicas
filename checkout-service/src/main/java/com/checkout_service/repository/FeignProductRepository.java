b00688ba304756c9bef7b1886057cbe1d2aaf313
Mateo <quotterimateo@gmail.com>
2025-03-14 21:28:47 -0300

    gateway funciona

--- checkout-service/src/main/java/com/checkout_service/repository/FeignProductRepository.java
+++ checkout-service/src/main/java/com/checkout_service/repository/FeignProductRepository.java
@@ -3,11 +3,10 @@ package com.checkout_service.repository;
 import com.checkout_service.entity.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @FeignClient(name="products-service", url="http://localhost:8080")
 public interface FeignProductRepository {
     @GetMapping("/products")
-    Product getProductById(@RequestParam String id);
+    Product getProductById(@RequestParam("id") String id);
 }
