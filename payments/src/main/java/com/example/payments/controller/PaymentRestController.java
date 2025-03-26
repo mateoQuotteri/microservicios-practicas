6f4e51a4b1b8c68b50de6aaeb69b5001cf86dd82
Mateo <quotterimateo@gmail.com>
2025-03-25 22:57:47 -0300

    centralizando logs con elasticsearch, logstash y kibana

--- payments/src/main/java/com/example/payments/controller/PaymentRestController.java
+++ payments/src/main/java/com/example/payments/controller/PaymentRestController.java
@@ -2,19 +2,26 @@ package com.example.payments.controller;
 
 import com.example.payments.entity.Payment;
 import com.example.payments.service.PaymentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/payments")
 @RestController
 public class PaymentRestController {
 
+    Logger logger = LoggerFactory.getLogger(PaymentRestController.class);
+
     private PaymentService paymentService;
 
     public PaymentRestController(PaymentService paymentService) {
         this.paymentService = paymentService;
+    }
+
+    @GetMapping("/{id}")
+    public Payment getPaymentById(String id){
+        logger.info("getPaymentById " + id);
+        return new Payment(id, 50.0f, "id2", "id3");
     }
 
     @PostMapping()
